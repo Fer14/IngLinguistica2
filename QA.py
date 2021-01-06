f4fbd3d95f06f40c5233897420587dba517b9bef
Fer <ferlobo1998@gmail.com>
2021-01-06 18:08:45 +0100

    Update QA.py

--- QA.py
+++ QA.py
@@ -1,12 +1,14 @@
 
 
+
+
 Ingredientes = ["huevos","mantequilla","leche","harina","azucar","sal"]
 
 
 
 print("Haz tu pregunta. Para acabar escribe SALIR")
-
 query = input().lower()
+
 
 while query != "salir":
 
@@ -24,39 +26,64 @@ while query != "salir":
         Respuesta = "cantidad"    
 
         if (query.find("huevos")!= -1):
+            Respuesta = "cantidad"
         if (query.find("mantequilla")!= -1):
+            Respuesta = "cantidad"
         if (query.find("leche")!= -1):
+            Respuesta = "cantidad"
         if (query.find("harina")!= -1):
+            Respuesta = "cantidad"
         if (query.find("azucar")!= -1):
+            Respuesta = "cantidad"
         if (query.find("sal")!= -1):
+            Respuesta = "cantidad"
 
+    elif (((query.find("cuanto") != -1) and (query.find("tiempo"))) or (query.find("cuando")!= -1)) and (any(ingredients in query for ingredients in Ingredientes)):
+        Respuesta = "tiempo"    
+
+        if (query.find("huevos")!= -1):
+            Respuesta = "tiempo" 
+        if (query.find("mantequilla")!= -1):
+            Respuesta = "tiempo" 
+        if (query.find("leche")!= -1):
+            Respuesta = "tiempo" 
+        if (query.find("harina")!= -1):
+            Respuesta = "tiempo" 
+        if (query.find("azucar")!= -1):
+            Respuesta = "tiempo" 
+        if (query.find("sal")!= -1):
+            Respuesta = "tiempo" 
 
     elif ((query.find("quien") != -1) or (query.find("autor")!= -1)) and (query.find("receta")!= -1):
         Respuesta = "autor"
 
     elif ((query.find("cuales") != -1) or (query.find("que")!= -1)) and (query.find("pasos")!= -1):
         Respuesta = "preparacion"
-
-
-    elif ((query.find("cuanta") != -1) or (query.find("cantidad")!= -1)) and (any(ingredients in query for ingredients in Ingredientes)):
-        Respuesta = "cantidad"    
+            
+    elif (query.find("que") != -1) and ((query.find("hacer")) or (query.find("hace")!= -1)) and (any(ingredients in query for ingredients in Ingredientes)):
+        Respuesta = "preparacion"    
 
         if (query.find("huevos")!= -1):
+            Respuesta = "preparacion" 
         if (query.find("mantequilla")!= -1):
+            Respuesta = "preparacion" 
         if (query.find("leche")!= -1):
+            Respuesta = "preparacion" 
         if (query.find("harina")!= -1):
+            Respuesta = "preparacion" 
         if (query.find("azucar")!= -1):
+            Respuesta = "preparacion" 
         if (query.find("sal")!= -1):
+            Respuesta = "preparacion" 
             
 
-
-    elif ((query.find("consejos") != -1) or (query.find("recomiendas")!= -1):
+    elif ((query.find("consejos") != -1) or (query.find("recomiendas")!= -1)):
         Respuesta = "consejos"        
 
-    elif ((query.find("utensilios") != -1):
+    elif ((query.find("utensilios") != -1)):
         Respuesta = "utensilios"
 
-    elif ((query.find("acompañamientos") != -1) or (query.find("añadir")!= -1):
+    elif ((query.find("acompañamientos") != -1) or (query.find("añadir")!= -1)):
         Respuesta = "acompañamientos"
 
        
@@ -66,6 +93,6 @@ while query != "salir":
     print("Haz tu pregunta. Para acabar escribe SALIR")
 
     query = input().lower()
-
+    
 
 
